d409c13ccd55efcb5dd9706ef34754e6429d4aef
matheus lopes <matheuslopes.developer@gmail.com>
2020-11-24 20:34:06 -0300

    solve the resistors problem

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,8 @@
+module ResistorColorDuo
+	RESISTOR_COLORS = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4,
+											green: 5, blue: 6, violet: 7, grey: 8, white: 9 }.freeze
+
+  def self.value(colors)
+    RESISTOR_COLORS[colors[0].to_sym] * 10 + RESISTOR_COLORS[colors[1].to_sym]
+  end
+end
